7d35b8de700a98e76045013ea4d562dbcc1dc9c9
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-04-28 15:12:59 +0300

    Add method setUp and refactor test_task_str in ModelTests

--- task_manager/tests/test_models.py
+++ task_manager/tests/test_models.py
@@ -8,18 +8,20 @@ from task_manager.models import Tag, Task
 
 class ModelTests(TestCase):
 
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            username="testuser",
+            password="testpass12345"
+        )
+
     def test_tag_str(self) -> None:
         test_tag = Tag.objects.create(name="test_tag")
         self.assertEqual(str(test_tag), "test_tag")
 
     def test_task_str(self) -> None:
-        user = get_user_model().objects.create_user(
-            username="testuser",
-            password="testpass12345"
-        )
         task = Task.objects.create(
             content="Test content",
-            user=user
+            user=self.user
         )
         self.assertEqual(
             str(task),
