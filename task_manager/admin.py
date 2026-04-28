26623b685bea8c9dd3f46ba42c92528f43aa922b
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-04-28 16:16:36 +0300

    Add custom model admin TaskAdmin to register model Task

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 
-from task_manager.models import Tag
+from task_manager.models import Tag, Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    empty_value_display = "Absent"
+    list_display = (
+        "content",
+        "created_at",
+        "deadline",
+        "get_status",
+    )
+
+    @admin.display(description="Status")
+    def get_status(self, task: Task) -> str:
+        return task.has_done
 
 
 admin.site.register(Tag)
