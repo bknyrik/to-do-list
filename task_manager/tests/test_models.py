b4fd3e8c256c8b7b069d0f5864f2bd174d65207c
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-04-28 14:48:24 +0300

    Add test case ModelTests with test test_tag_str

--- task_manager/tests/test_models.py
+++ task_manager/tests/test_models.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+from task_manager.models import Tag
+
+
+class ModelTests(TestCase):
+
+    def test_tag_str(self) -> None:
+        test_tag = Tag.objects.create(name="test_tag")
+        self.assertEqual(str(test_tag), "test_tag")
