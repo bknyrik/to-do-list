56e8f9ede474f762cae20113fb8db76a1f19110e
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-04-28 15:10:59 +0300

    Add test test_task_str to ModelTests

--- task_manager/tests/test_models.py
+++ task_manager/tests/test_models.py
@@ -1,6 +1,9 @@
-from django.test import TestCase
+from datetime import datetime
 
-from task_manager.models import Tag
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from task_manager.models import Tag, Task
 
 
 class ModelTests(TestCase):
@@ -8,3 +11,17 @@ class ModelTests(TestCase):
     def test_tag_str(self) -> None:
         test_tag = Tag.objects.create(name="test_tag")
         self.assertEqual(str(test_tag), "test_tag")
+
+    def test_task_str(self) -> None:
+        user = get_user_model().objects.create_user(
+            username="testuser",
+            password="testpass12345"
+        )
+        task = Task.objects.create(
+            content="Test content",
+            user=user
+        )
+        self.assertEqual(
+            str(task),
+            f"Test content {datetime.now()} {task.has_done}"
+        )
