94fe936e0816ce2d2056bba2719f356aa068c486
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-04-28 11:38:42 +0300

    Add model Task

--- task_manager/models.py
+++ task_manager/models.py
@@ -9,3 +9,11 @@ class Tag(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True)
+    completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
