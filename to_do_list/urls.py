a4fe76501a5bef27f18b60c483578d8a0e600890
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-04-29 17:07:37 +0300

    Add namespace with debug_toolbar urls to urlpatterns in project urls.py

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("task_manager.urls", namespace="task_manager")),
+    path("__debug__/", include("debug_toolbar.urls"))
 ]
