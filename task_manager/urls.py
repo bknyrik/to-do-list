55ef86a193c13010be5a7e802fd7d106aa8fb933
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-04-29 11:43:23 +0300

    Add path with TagDeleteView to urlpatterns of task_manager urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from task_manager.views import (
     TagListView,
     TagCreateView,
-    TagUpdateView
+    TagUpdateView,
+    TagDeleteView,
 )
 
 
@@ -11,6 +12,7 @@ urlpatterns = [
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "task_manager"
