3e79a219f43547a15582b20ef8f768b210b84c69
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-04-29 11:39:52 +0300

    Add path with TagUpdateView to urlpatterns of task_manager urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from task_manager.views import (
     TagListView,
     TagCreateView,
+    TagUpdateView
 )
 
 
 urlpatterns = [
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
 ]
 
 app_name = "task_manager"
