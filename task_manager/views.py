766d2183f2ae362d4e124330aaf394c922319b19
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-04-29 11:31:11 +0300

    Add view TagCreateView

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,7 +1,13 @@
 from django.views import generic
+from django.urls import reverse_lazy
 
 from task_manager.models import Tag
 
 
 class TagListView(generic.ListView):
     model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    success_url = reverse_lazy("task_manager:tag-list")
