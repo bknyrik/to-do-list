ae41adb2ca3f20249a9b260f9079b4716a84fa37
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-04-29 11:14:06 +0300

    Add view TagListView

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from task_manager.models import Tag
+
+
+class TagListView(generic.ListView):
+    model = Tag
