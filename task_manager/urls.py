298836aa07c2588a8318056f35c8132c3fdef081
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-04-29 11:18:26 +0300

    Add path with TagListView to urlpatterns of task_manager urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
+from task_manager.views import (
+    TagListView,
+)
+
 
 urlpatterns = [
-
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "task_manager"
