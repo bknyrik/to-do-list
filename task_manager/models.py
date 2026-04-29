63bac361d799e90e1f897db8ee181ed964ebe865
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-04-29 11:01:24 +0300

    Add option blank to field deadline of Task

--- task_manager/models.py
+++ task_manager/models.py
@@ -23,7 +23,7 @@ class Tag(models.Model):
 class Task(models.Model):
     content = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    deadline = models.DateTimeField(null=True)
+    deadline = models.DateTimeField(null=True, blank=True)
     completed = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="tasks")
     user = models.ForeignKey(
