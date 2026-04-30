db58e6fa3453c309993c4c21787de41795af4488
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-04-30 11:03:45 +0300

    Add module forms.py with form class RegisterUserForm

---
+++ task_manager/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import BaseUserCreationForm
+from django.contrib.auth import get_user_model
+
+
+class RegisterUserForm(BaseUserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password1",
+            "password2",
+        )
