445f20c0f303657fc23f7423ceef7ecf521b9227
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-01 20:00:53 +0300

    Add field deadline to TaskForm

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -6,6 +6,12 @@ from task_manager.models import Task
 
 
 class TaskForm(forms.ModelForm):
+
+    deadline = forms.DateTimeField(
+        required=False,
+        widget=forms.DateTimeInput(attrs={"type": "datetime-local"})
+    )
+
     class Meta:
         model = Task
         fields = ("content", "deadline", "tags")
