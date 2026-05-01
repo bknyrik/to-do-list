79af21ad73b92bf0ba49e2514e9356cefd729f2f
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-02 00:38:41 +0300

    Add paths with static urls to urlpatterns of project urls.py

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("task_manager.urls", namespace="task_manager")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__/", include("debug_toolbar.urls"))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
