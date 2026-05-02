1fdbc591c19be947eb95167c7c4f25fc98e96ef9
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-02 11:24:48 +0300

    Add test test_register_user to UnauthorizedTests

--- task_manager/tests/test_views.py
+++ task_manager/tests/test_views.py
@@ -1,11 +1,13 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 HTTP_200_OK = 200
 
 TAG_LIST_URL = reverse("task_manager:tag-list")
 TASK_LIST_URL = reverse("task_manager:task-list")
+USER_CREATE_URL = reverse("task_manager:user-create")
 
 
 class UnauthorizedTests(TestCase):
@@ -17,3 +19,14 @@ class UnauthorizedTests(TestCase):
     def test_task_list_login_required(self) -> None:
         response = self.client.get(TASK_LIST_URL)
         self.assertNotEqual(response.status_code, HTTP_200_OK)
+
+    def test_register_user(self) -> None:
+        data = {
+            "username": "testuser",
+            "password1": "testpass",
+            "password2": "testpass",
+        }
+
+        self.client.post(USER_CREATE_URL, data=data)
+        user = get_user_model().objects.get(username=data["username"])
+        self.assertTrue(user.check_password(data["password1"]))
