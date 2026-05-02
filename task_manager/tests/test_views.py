00a26d280b5ab8b101a16ba99dd49667d98b2168
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-02 10:41:57 +0300

    Add test test_task_list_login_required to UnauthorizedTests

--- task_manager/tests/test_views.py
+++ task_manager/tests/test_views.py
@@ -5,10 +5,15 @@ from django.urls import reverse
 HTTP_200_OK = 200
 
 TAG_LIST_URL = reverse("task_manager:tag-list")
+TASK_LIST_URL = reverse("task_manager:task-list")
 
 
 class UnauthorizedTests(TestCase):
 
     def test_tag_list_login_required(self) -> None:
         response = self.client.get(TAG_LIST_URL)
+        self.assertNotEqual(response.status_code, HTTP_200_OK)
+
+    def test_task_list_login_required(self) -> None:
+        response = self.client.get(TASK_LIST_URL)
         self.assertNotEqual(response.status_code, HTTP_200_OK)
