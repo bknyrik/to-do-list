47dbcdfcdd27a5fc9730ce3b5ec26643584a3c59
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-02 11:31:42 +0300

    Add test case AuthorizedUserTests with method setUp and test test_tag_create_forbidden

--- task_manager/tests/test_views.py
+++ task_manager/tests/test_views.py
@@ -1,11 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 
 HTTP_200_OK = 200
+HTTP_403_OK = 403
 
 TAG_LIST_URL = reverse("task_manager:tag-list")
+TAG_CREATE_URL = reverse("task_manager:tag-create")
 TASK_LIST_URL = reverse("task_manager:task-list")
 USER_CREATE_URL = reverse("task_manager:user-create")
 
@@ -30,3 +32,18 @@ class UnauthorizedTests(TestCase):
         self.client.post(USER_CREATE_URL, data=data)
         user = get_user_model().objects.get(username=data["username"])
         self.assertTrue(user.check_password(data["password1"]))
+
+
+class AuthorizedUserTests(TestCase):
+
+    def setUp(self) -> None:
+        self.client = Client()
+        self.user = get_user_model().objects.create_user(
+            username="testuser",
+            password="testpass"
+        )
+        self.client.force_login(user=self.user)
+
+    def test_tag_create_forbidden(self) -> None:
+        response = self.client.get(TAG_CREATE_URL)
+        self.assertEqual(response.status_code, HTTP_403_OK)
